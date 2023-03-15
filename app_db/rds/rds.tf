ddefeec3e61ffd07452e615d327e9291e044da61
iman <bekoviman9@gmail.com>
2023-03-15 19:57:30 +0000

    app-db

---
+++ app_db/rds/rds.tf
@@ -0,0 +1,13 @@
+module "rds_db" {
+  source                   = "../../modules/db_module/"
+  allocated_storage        = 10
+  db_name                  = "myrds"
+  engine                   = "mysql"
+  engine_version           = "5.7"
+  instance_class           = "db.t3.micro"
+  username                 = "ibekov"
+  password                 = "ibekov2002"
+  db_subnet_group_name     = "main"
+  vpc_security_group_ids   = ["sg-0482a5cace35cba28"]
+  skip_final_snapshot      = true
+}
